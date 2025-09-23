ea4aa4549fa25b7c70395889887f0970696148f0
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-24 00:02:25 +0530

    pipa: Initial import
    Change-Id: I74d0b5347e9d03d6e9d734a215266515adc5fec9

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),pipa)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,e2c9a77c8257ae8535022efba0e9cf2e76b3a3ea)
+$(call add-radio-file-sha1-checked,radio/aop.img,4d34fe45097047454ff07d893841bcaf74c21c57)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ee8676f9cab1f920b06384d537de89a3dfc9679f)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,7e6826e69df6e7ff40aee32402f365d1231d2670)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,610126d471f3aa85226f8d983a97b0bf2a72d9ef)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,8c4db037d7ea157a97942f7f56d3f334ca8f0d5b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,e78cc34567eba97000f91f8577be0ad9e14d5de9)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,24d3859a1a12b7e8a58bd722741d5bacd76d1ea6)
+$(call add-radio-file-sha1-checked,radio/hyp.img,cda6a635115001868ed6d21c4630057830baee07)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,1524ae34561d379b7f23847f50e3d6ccaf4baaa2)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,18103128f8e6bf64a94bd1e378e8a413e8bf9885)
+$(call add-radio-file-sha1-checked,radio/modem.img,232659d51fc727d4f7b61a795ee5ec39b7180131)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,03e95b6f96af39248ee290d9e064c69040aab6eb)
+$(call add-radio-file-sha1-checked,radio/tz.img,264eec292eb83614599b68c0e76205ef056b1e7e)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,26d3a3d512565c17fdb6172382a39947ec2b88a7)
+$(call add-radio-file-sha1-checked,radio/xbl.img,dc62dbee23f2479955587860e64d3dc44e39bcfb)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,1e2c2831d1e983afe04d116d3a7844ce8b94dc0c)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
